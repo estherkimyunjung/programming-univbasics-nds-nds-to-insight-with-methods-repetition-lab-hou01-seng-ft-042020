5c116204aae6239d323c052ee11aeff47eb87374
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-03-25 22:32:32 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -51,7 +51,7 @@ def total_gross(source)
   directors_index = 0
 
   while directors_index < list_of_directors(source).length do
-    total += directors_totals(source)[list_of_directors(source)]
+    total += directors_totals(source)[list_of_directors(source)[directors_index]]
 
     directors_index += 1 
   end
